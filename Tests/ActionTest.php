97397a63338223bf432336c451e2b4fa3931d2e0
Joshua Parker <josh@joshuaparker.blog>
2021-09-23 10:53:08 -0700

    Updated file headers.

--- Tests/ActionTest.php
+++ Tests/ActionTest.php
@@ -4,7 +4,8 @@
  * Qubus\EventDispatcher
  *
  * @link       https://github.com/QubusPHP/event-dispatcher
- * @copyright  2020 Joshua Parker
+ * @copyright  2020 Joshua Parker <josh@joshuaparker.blog>
+ * @copyright  2018 Filip Štamcar (original author Tor Morten Jensen)
  * @license    https://opensource.org/licenses/mit-license.php MIT License
  *
  * @since      1.0.0
